47ba1ef7b1ef837f9b5a1555df1ef78a82ac678f
12874-fn <12874@holbertonstudents.com>
2026-04-28 19:15:15 -0600

    Update 0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- Creates a table users with unique email
+-- Creates a users table with unique email
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
@@ -6,6 +6,20 @@ CREATE TABLE IF NOT EXISTS users (
     PRIMARY KEY (id),
     UNIQUE (email)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
