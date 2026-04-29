3f434d240a41b6685ea82311baea7c71322f4fb1
12874-fn <12874@holbertonstudents.com>
2026-04-28 19:11:41 -0600

    Delete MySQL_Advanced/0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- Creates a users table with unique email
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
-
