a1a56275dce373c0e4d71e22b321a124ccfb7e5e
12874-fn <12874@holbertonstudents.com>
2026-04-28 19:09:35 -0600

    Create 10-div.sql

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- Creates a function SafeDiv that safely divides two integers
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END//
+
+DELIMITER ;
+
+
+
+
+
